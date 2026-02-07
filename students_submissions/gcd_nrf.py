e5ad56921df47d1361d3b6e0b8d8153391f387d0
Nona Fulmore Harris <nonarharris@gmail.com>
2026-02-06 19:59:08 -0500

    Handled negatives and zero values

--- students_submissions/gcd_nrf.py
+++ students_submissions/gcd_nrf.py
@@ -1,30 +1,37 @@
 '''
-
 Nona Harris
 CS-490-102
 Professor McCann
 06 February 2026
 
-gcd_nrf.py:
-    - Calculate the greatest common divisor (GCD) of two integers a and b
-    using the Euclidean algorithm.
-
+Calculate the greatest common divisor (GCD) of two integers a and by
+using the Euclidean algorithm.
 '''
 
 def gcd(a: int, b: int) -> int:
-    #In the case of negative numbers
-    if a < 0 or b < 0:
+    #In case of negative numbers
+    a,b = abs(a), abs(b)
+
+    #In the case of A and B being 0:
+    if a == 0 and b == 0:
         print("Error: Negative value(s) ", end="")
         return None
 
+    #In the case of A or B not being integers:
+    if not isinstance(a, int) or not isinstance(b, int):
+        print("Error: Non-integer value(s) ", end="")
+        return None
+    
     if b == 0:
         return a
     return gcd(b, a % b)
 
-#Test Cases
-print(gcd(56, 49)) #Should return 7
-print(gcd(100, 90)) #Should return 10
-print(gcd(-4, -2)) #Should print Error message
-print(gcd(7, 9)) #Should return 1
-print(gcd(2000, 4000)) #Should return 2000
-print(gcd(0, 9)) #Should return 9
+
+if __name__ == "__main__":
+    #Test Cases
+    print(gcd(56, 49)) #Should return 7
+    print(gcd(100, 90)) #Should return 10
+    print(gcd(-4, -2)) #Should return 2
+    print(gcd(7, 9)) #Should return 1
+    print(gcd(2000, 4000)) #Should return 2000
+    print(gcd(0, 9)) #Should return 9
